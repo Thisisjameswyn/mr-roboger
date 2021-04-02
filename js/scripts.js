52661cd19f6a09e5f6d2be724bafa9f1d0e4d61e
James Wyn <thisisjameswyn@gmail.com>
2021-04-02 10:45:38 -0700

    add fourth test functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 function storyTime(userNum) {
   let storyArray = [];
   for(let i = 0; i <= userNum; i++) {
-    if(i>toString().includes("2")) {
+    if(i.toString().includes("3")) {
+      storyArray.push("Won't you be my neighbor?");
+    } else if(i.toString().includes("2")) {
       storyArray.push("Boop!");
     } else if(i.toString().includes("1")) {
       storyArray.push("Beep!");
