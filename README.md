051afbf8020d1ddbef6928aa13dceb0529e01914
James Wyn <thisisjameswyn@gmail.com>
2021-04-02 08:53:41 -0700

    add additional files

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Describe: storyTime()
 Test: "It should return an array with a 0 if the number 0 is inputted"
 Code: storyTime(0)
 Expected Output: [0]
+
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function storyTime(userNum) {
+  let storyArray = [];
+  for(let i = 0; i < userNum; i++) {
+    storyArray.push(i.toString());
+  }
+  return storyArray;
+}
